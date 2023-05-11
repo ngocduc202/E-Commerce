3978a86230923d3c81fa58beb98f012221f9e535
ngocduc202 <ngocduc1542002@gmail.com>
2023-05-11 22:24:03 +0700

    #3 login and register

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,13 +8,45 @@ const register  = asyncHandler(async (req , res) => {
     sucess : false,
     mes : "Missing puts"
   })
-  const response = await User.create(req.body)
-  return res.status(200).json({
-    sucess : response ? true : false,
-    response
-  })
+
+  const user = await User.findOne({email})
+  if(user)
+          throw new Error("User has existed!")
+  else{
+    const newUser = await User.create(req.body)
+    return res.status(200).json({
+      sucess : newUser ? true : false ,
+      mes : newUser ?  "Register is successfully . Please go login " : "Something went wrong"
+    })
+  }
+
 })
 
+const login  = asyncHandler(async (req , res) => {
+  const {email , password } = req.body
+  if(!email || !password)
+  return res.status(400).json({
+    sucess : false,
+    mes : "Missing puts"
+  })
+
+  const  response = await User.findOne({email})
+  if(response && await response.isCorrectPassword(password)){
+    const {password , role , ...userData} = response.toObject()
+      return res.status(200).json({
+        sucess : true ,
+        userData
+      })
+  }
+  else{
+    throw new Error("Invalid credentials!")
+  }
+
+
+})
+
+
 module.exports = {
-  register
+  register,
+  login
 }
