6341d9ec1d156f1fd51267130d3eaad40de6a935
ngocduc202 <ngocduc1542002@gmail.com>
2023-07-18 21:10:38 +0700

    18/07/2023

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React ,{useState , useEffect} from 'react'
+import {apiGetCategories} from '../apis/app'
+import { NavLink } from 'react-router-dom'
+import {createSlug} from '../ultils/helpers'
 
 const SideBar = () => {
+
+  const [categories, setCategories] = useState(null)
+
+  const fetchCategories = async () => {
+    const response = await apiGetCategories()
+    if(response.success) {
+      setCategories(response.productCategories)
+    }
+  }
+  useEffect(() => {
+    fetchCategories()
+  }, [])
   return (
-    <div>SideBar</div>
+    <div>
+      {categories?.map(el => (
+        <NavLink
+          key={createSlug(el.title)}
+          to={createSlug(el.title)}
+        >
+            {el.title}
+        </NavLink>
+      ))}
+    </div>
   )
 }
 
---
+++ client/src/ultils/helpers.js
@@ -0,0 +1,2 @@
+export const createSlug = string => string.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").split(' ').join('-')
+
